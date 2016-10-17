ce9a586e5a9a8399c019bdda31e1cb6022ed6800
nhat hoang <hoangnh092185@gmail.com>
2016-10-16 22:02:44 -0700

    add completeness.pipe component.

---
+++ app/completeness.pipe.ts
@@ -0,0 +1,37 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Task } from './task.model';
+
+
+@Pipe({
+  name: "completeness",
+  pure: true
+})
+
+export class CompletenessPipe implements PipeTransform {
+  transform(input: Task[], info) {
+    var desiredCompleteness = info[0];
+    console.log(info);
+    console.log('desiredCompleteness = ' + info[0]);
+    console.log('number of slices of cheesecake: ' + info[3]);
+
+    ////////////////////////
+    var output: Task[] = [];
+    if(desiredCompleteness === "notDone") {
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].done === false) {
+          output.push(input[i]);
+        }
+      }
+      return output;
+    } else if (desiredCompleteness === "isDone") {
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].done === true) {
+          output.push(input[i]);
+        }
+      }
+      return output;
+    } else {
+      return input;
+    }
+  }
+}
--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -1,19 +1,31 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from './task.model';
+import { CompletenessPipe } from './completeness.pipe';
 
 @Component({
   selector: 'task-list',
+  // pipes: 'completeness',
   template: `
-    <div class="jumbotron" *ngFor="let currentTask of childTaskList">
+    <div *ngFor="let currentTask of childTaskList | completeness:selectedCompleteness">
       <h3>{{ currentTask.description }}  <button class="btn btn-primary" (click)="editButtonHasBeenClicked(currentTask)">Edit</button></h3>
     </div>
+    <select (change)="onChange($event.target.value)" class="filter">
+      <option value="all">Show All</option>
+      <option value="isDone">Show Done</option>
+      <option value="notDone" selected="selected">Show Not Done</option>
+    </select>
   `
 })
 
 export class TaskListComponent {
+  public selectedCompleteness: string = "notDone";
   @Input() childTaskList: Task[];
   @Output() clickSender = new EventEmitter();
   editButtonHasBeenClicked(taskToEdit: Task) {
     this.clickSender.emit(taskToEdit);
+  }
+  onChange(optionFromMenu) {
+    this.selectedCompleteness = optionFromMenu;
+    console.log(this.selectedCompleteness);
   }
 }
